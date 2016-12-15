f6ffd7a3d3ff80994e22fda56622a807995904a5
GRRook <gerbenrook@gmail.com>
2016-12-15 15:38:47 +0100

    xml
    xml

--- BijwerkenGebruikers1/src/Main.java
+++ BijwerkenGebruikers1/src/Main.java
@@ -27,68 +27,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class Main {
-	private static String xml = "<data><GetAuthenticatedUserResponse> <tuple>\r\n" + 
-			"        <old>\r\n" + 
-			"        <entry dn=\"cn=u00z1f2@mod.nl,cn=authenticated users,cn=cordys,cn=ontwikkel,o=mod.nl\" entryUUID=\"4498cb81-3d7c-4e37-b9b2-b2c676f87bd2\">\r\n" + 
-			"          <defaultcontext>\r\n" + 
-			"            <string>o=grook,cn=cordys,cn=ontwikkel,o=mod.nl</string>\r\n" + 
-			"          </defaultcontext>\r\n" + 
-			"          <busextension>\r\n" + 
-			"            <string><userprofile xmlns=\"http://common.schemas.bop.mindef.nl/1.0/userprofile\"><medewerkerid>00000319780</medewerkerid><uaccount>u00z1f2</uaccount></userprofile></string>\r\n" + 
-			"          </busextension>\r\n" + 
-			"          <userPassword>\r\n" + 
-			"            <string>{SHA1}Ru75sxqxM88xNNigHu95ZtuNpLc=</string>\r\n" + 
-			"          </userPassword>\r\n" + 
-			"          <description>\r\n" + 
-			"            <string>u00z1f2@mod.nl</string>\r\n" + 
-			"          </description>\r\n" + 
-			"          <osidentity>\r\n" + 
-			"            <string>u00z1f2@mod.nl</string>\r\n" + 
-			"          </osidentity>\r\n" + 
-			"          <cn>\r\n" + 
-			"            <string>u00z1f2@mod.nl</string>\r\n" + 
-			"          </cn>\r\n" + 
-			"          <authenticationtype>\r\n" + 
-			"            <string>custom</string>\r\n" + 
-			"          </authenticationtype>\r\n" + 
-			"          <objectclass>\r\n" + 
-			"            <string>top</string>\r\n" + 
-			"            <string>busauthenticationuser</string>\r\n" + 
-			"          </objectclass>\r\n" + 
-			"        </entry>\r\n" + 
-			"      </old>\r\n" + 
-			"    </tuple>\r\n" + 
-			"                    <tuple>\r\n" + 
-			"      <old>\r\n" + 
-			"        <entry dn=\"cn=u00r699@mod.nl,cn=authenticated users,cn=cordys,cn=ontwikkel,o=mod.nl\" entryUUID=\"6419cde8-acf7-4b7d-9e18-a6f1a84cc1ae\">\r\n" + 
-			"          <defaultcontext>\r\n" + 
-			"            <string>o=Verlof 2.0,cn=cordys,cn=ontwikkel,o=mod.nl</string>\r\n" + 
-			"          </defaultcontext>\r\n" + 
-			"          <busextension>\r\n" + 
-			"            <string><userprofile xmlns=\"http://common.schemas.bop.mindef.nl/1.0/userprofile\"><medewerkerid>00000294173</medewerkerid><uaccount>u00r699</uaccount></userprofile></string>\r\n" + 
-			"          </busextension>\r\n" + 
-			"          <userPassword>\r\n" + 
-			"            <string>{SHA1}o3XVSLf7mjNsqiLIjLSma0mahNQ=</string>\r\n" + 
-			"          </userPassword>\r\n" + 
-			"          <description>\r\n" + 
-			"            <string>u00r699@mod.nl</string>\r\n" + 
-			"          </description>\r\n" + 
-			"          <osidentity>\r\n" + 
-			"            <string>u00r699@mod.nl</string>\r\n" + 
-			"          </osidentity>\r\n" + 
-			"          <cn>\r\n" + 
-			"            <string>u00r699@mod.nl</string>\r\n" + 
-			"          </cn>\r\n" + 
-			"          <authenticationtype>\r\n" + 
-			"            <string>custom</string>\r\n" + 
-			"          </authenticationtype>\r\n" + 
-			"          <objectclass>\r\n" + 
-			"            <string>top</string>\r\n" + 
-			"            <string>busauthenticationuser</string>\r\n" + 
-			"          </objectclass>\r\n" + 
-			"        </entry>\r\n" + 
-			"      </old>\r\n" + 
-			"    </tuple></GetAuthenticatedUserResponse></data>";
+	private static String xml = "";
 	
 	static Node temp;
 	
